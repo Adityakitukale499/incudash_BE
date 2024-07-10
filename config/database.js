b16bb003f5c88d990ed41b7f84469b3b9c3c5f1e
Aditya kitukale <adityakitukale4599@gmail.com.com>
2024-07-10 20:44:35 +0530

    added commit

--- config/database.js
+++ config/database.js
@@ -1,15 +1,37 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'mongoose',
+//       settings: {
+//         host: env('DATABASE_HOST', 'incudash.bxjl34c.mongodb.net'),
+//         srv: env.bool('DATABASE_SRV', true),
+//         port: env.int('DATABASE_PORT', 27017),
+//         database: env('DATABASE_NAME', 'incudash'),
+//         username: env('DATABASE_USERNAME', 'parassethprojects'),
+//         password: env('DATABASE_PASSWORD', 'uJf8ZfmlfgWBoN8v'),
+//       },
+//       options: {
+//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+//         ssl: env.bool('DATABASE_SSL', true),
+//       },
+//     },
+//   },
+// });
+
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'incudash.bxjl34c.mongodb.net'),
+        host: env('DATABASE_HOST', 'cluster0.8ekoj3s.mongodb.net'),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'incudash'),
-        username: env('DATABASE_USERNAME', 'parassethprojects'),
-        password: env('DATABASE_PASSWORD', 'uJf8ZfmlfgWBoN8v'),
+        database: env('DATABASE_NAME', 'Cluster0'),
+        username: env('DATABASE_USERNAME', 'adityakitukale4599'),
+        password: env('DATABASE_PASSWORD', 'Aditya-4599'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
